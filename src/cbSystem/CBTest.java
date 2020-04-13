256824412485575ee0108c370b813fffda446667
nathanfeeney <tnfeeney@hotmail.com>
2020-04-13 15:56:55 +0100

    Added Case: 0

--- src/cbSystem/CBTest.java
+++ src/cbSystem/CBTest.java
@@ -6,42 +6,66 @@ public class CBTest {
 
     public static void main(String[] args) throws Exception {
         SortedADT sorted = new BinarySearchTree();
-        sorted.insert("bombsell");
+        sorted.insert("bombsHell");
         sorted.insert("kingsmen");
         sorted.insert("jojo Rabit");
-        sorted.insert("X-Calibre");
-        sorted.insert("Pineapple Express");
+        sorted.insert("x-alibre");
+        sorted.insert("pineapple express");
         sorted.insert("xmen");
-        sorted.insert("b");
-        sorted.insert("c");
-        sorted.insert("d");
-        sorted.insert("e");
+        sorted.insert("1917");
+        sorted.insert("hustlers");
+        sorted.insert("dumbo");
+        sorted.insert("james bond");
         // Test Inserts
         
-        System.out.println(sorted);// This prints the populated tree
-        String value;
+       // System.out.println(sorted);// This prints the populated tree
+        String movieTitle;
         Integer option;
+        String exit;
         do{
+        	System.out.println("Please Selct one of thse options.");
+        	System.out.println("1 :Movie Catelogue");
+        	System.out.println("2 :Search for movie");
+        	System.out.println("0 :Exit");
         	option = Input.getInteger("option: ");
         	switch(option) {
-        	
+
         	case 1:
-        		System.out.println("You have chose the first option");
+        		System.out.println("You have chosen the first option");
         		break;
         	case 2:
 		        	do {
-		        		value = Input.getString("value: ");
+		        		movieTitle = Input.getString("Movie Title: ").toLowerCase();
 		                try {
-		                    sorted.find(value);
-		                    System.out.println("found");
+		                    sorted.find(movieTitle);
+		                    System.out.println("Movie Found");
 		                } catch (SortedADT.NotFoundException e) {
-		                    System.out.println("value not found");
+		                    System.out.println("Movie not found: " + movieTitle);
 		                }
 		            }while(Repeat.repeat());//End of do/while loop 
             break;
+            
+        	case 0:
+        		System.out.print("Are you sure you want to exit? ");
+        		//System.out.println();
+        		do {
+        			exit = Input.getString("Y/N: ").toLowerCase();
+            		if(exit == "Y" ||exit == "y") {
+            			System.exit(0);
+            			System.out.println("Goodbye");
+            			
+            		}else if(exit == "N" ||exit == "n") {
+            			System.out.println("Returning to main menu");
+            			break;
+            		}
+        		}while(exit == "n" || exit == "y");
         		
+            break;
+        
         	default:
+        		
         		System.out.println("this didnt wurk");
+        		
         		break;
         	
         	}
---
+++ src/cbSystem/MovieDetails.java
@@ -0,0 +1,18 @@
+package cbSystem;
+
+public class MovieDetails {
+	public static void main(String[] args) throws Exception{
+		 SortedADT movieTree = new BinarySearchTree();
+		 	movieTree.insert("bombsell");
+		 	movieTree.insert("kingsmen");
+		 	movieTree.insert("jojo Rabit");
+		 	movieTree.insert("X-Calibre");
+		 	movieTree.insert("Pineapple Express");
+		 	movieTree.insert("xmen");
+		 	movieTree.insert("b");
+		    movieTree.insert("c");
+		    movieTree.insert("d");
+		    movieTree.insert("e");
+		}
+	}
+   
